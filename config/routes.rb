ef1072d1affe5d93d2e3174c6721c0d2c2daebd3
sreeprasad <sreeprasad.sp@gmail.com>
2014-06-08 08:11:12 -0400

    generated user and favorite model and user and favorite controller and protected from forgery

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'popcorn#index'
   get 'popcorn/index'
+  
+  resources :movies
+  resources :favorites
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
